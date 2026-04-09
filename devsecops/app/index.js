af4cafe617c8ef9bdd44a3632b5e462a30834d03
Ezra Bolt <ez.bolt@st.hanze.nl>
2026-04-09 09:20:56 +0000

    fix port to 3001

--- devsecops/app/index.js
+++ devsecops/app/index.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
+const PORT = 3001;
+
 const server = http.createServer((req, res) => {
   res.end("Hello DevSecOps!");
 });
 
-server.listen(3000);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
