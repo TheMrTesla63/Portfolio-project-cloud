77f72dab13d7bbba66a7c29b69311bfddb9bc396
admin <admin@local.local>
2026-04-20 15:53:38 +0000

    Added vulnerability

--- devsecops/app/index.js
+++ devsecops/app/index.js
@@ -1,13 +1,30 @@
-
-
 const http = require('http');
+const url = require('url');
 
 const PORT = 3001;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello DevSecOps! v2 dit is een test");
+  const queryObject = url.parse(req.url, true).query;
+  const name = queryObject.name || "";
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+
+  res.end(`
+    <html>
+      <body>
+        <h1>DevSecOps Demo App</h1>
+
+        <form method="GET">
+          <input type="text" name="name" placeholder="Enter your name">
+          <button type="submit">Submit</button>
+        </form>
+
+        <h2>Hello ${name}</h2>
+      </body>
+    </html>
+  `);
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(\`Server running on port \${PORT}\`);
 });
